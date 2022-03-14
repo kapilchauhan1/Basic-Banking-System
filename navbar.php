254de88067d248e6c23a7cf09f81d8cd323f364a
Kapil Chauhan <chauhankapil0000@gmail.com>
2022-03-14 12:30:02 +0530

    Initial commit

---
+++ navbar.php
@@ -0,0 +1,24 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light " style="background-color: #C5C6D0;">
+      <a class="navbar-brand" href="index.php" style="color : #495057;"><img src="img/bnklg.png" alt="Logo" width="26" style="margin-bottom:4px;"> </a>
+      <a class="navbar-brand" href="index.php" style="color : black;"><b>Home</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : black;"><b>Create User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="removeuser.php" style="color : black;"><b>Remove User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : black;"><b>Perform Transaction</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : black;"><b>Past Transaction </b></a>
+              </li>
+          </div>
+       </nav>
---
+++ navbar1.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-expand-lg navbar-light " style="background-color: #e3f2fd;">
+      <a class="navbar-brand" href="index.php" style="font-size:30px; padding-left:50px;"  style="color : #003366;"  ><img src="img/bank2.png" alt="Logo" width="80" height="70"  style="margin-bottom:4px;" > <b> BASIC BANKING SYSTEM</b></a>
+      
+      
+       </nav>
